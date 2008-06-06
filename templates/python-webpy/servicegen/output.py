ed605349b754c3bf577dd0762f1e3a4509013d6c
Patrice Neff <www-github@patrice.ch>
2008-06-06 12:31:10 +0200

    Correctly prioritize output types.

--- templates/python-webpy/servicegen/output.py
+++ templates/python-webpy/servicegen/output.py
@@ -1,13 +1,22 @@
 import web
 
-def matches(type):
-    """Returns true if the current request is of the given type."""
+def best_of(types):
+    """
+    Returns the best output type of `types' to fullfil the current request.
+    """
+    
+    # Path takes priority
+    for type in types:
+        if web.ctx.path.endswith('.' + type):
+            return type
+    
+    # Try accept header
     accept = [
         entry.split(';')[0].split('/')[-1] for entry in
             web.ctx.env.get('HTTP_ACCEPT').split(',')]
+    for type in accept:
+        if type in types:
+            return type
     
-    if web.ctx.path.endswith('.' + type):
-        return True
-    if type in accept:
-        return True
-    return False
+    # Return first entry (to be changed)
+    return types[0]
