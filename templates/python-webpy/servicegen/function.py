0d4162ebd824dcf0edafbe7e4f460c1883b122e5
Patrice Neff <www-github@patrice.ch>
2008-06-06 10:42:04 +0200

    Also support non-regexp values for validation

--- templates/python-webpy/servicegen/function.py
+++ templates/python-webpy/servicegen/function.py
@@ -1,7 +1,12 @@
 import web
 
 def validate(value, comparison):
-    if comparison.match(value) == None:
+    matches = True
+    try:
+        matches = (comparison.match(value) != None)
+    except AttributeError:
+        matches = (str(value) == str(comparison))
+    if not matches:
         web.ctx.status = '400 Bad Request'
         web.header('Content-Type', 'text/html')
         return web.output('Invalid value "' + value + '"\n')
